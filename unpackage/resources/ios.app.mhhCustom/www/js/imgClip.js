f1713a2b07b15a7e2a051967739f8447d9ede969
loserjohn <1292404844@qq.com>
2018-11-07 18:35:55 +0800

    11.7

---
+++ unpackage/resources/ios.app.mhhCustom/www/js/imgClip.js
@@ -0,0 +1,180 @@
+//需要剪裁类型
+(function(owner) {
+	owner.baseUrl = "https://mobile.mhh999.com"
+	/*选择图片*/
+	owner.getIMG = function(title, callback) {
+		if(mui.os.plus) {
+			var a = [{
+				title: '拍照'
+			}, {
+				title: '从手机相册选择'
+			}];
+			plus.nativeUI.actionSheet({
+				title: title,
+				cancel: '取消',
+				buttons: a
+			}, function(b) {
+
+				switch(b.index) {
+					case 0:
+						break;
+					case 1:
+						//拍照 
+
+						owner.cameraImages(function(src) {
+							if(callback) callback(src);
+						});
+
+						break;
+					case 2:
+						//打开相册 
+						owner.galleryImages(1, function(src) {
+							if(callback) callback(src);
+						});
+
+						break;
+					default:
+						break;
+				}
+			}, false);
+		}
+	}
+	/*选择图片2 ke取消*/
+	owner.getIMGPro = function(title, callback) {
+		if(mui.os.plus) {
+			var a = [{
+				title: '拍照'
+			}, {
+				title: '从手机相册选择'
+			}];
+			plus.nativeUI.actionSheet({
+				title: title,
+				cancel: '取消',
+				buttons: a
+			}, function(b) {
+
+				switch(b.index) {
+					case 0:
+						break;
+					case 1:
+						//拍照 
+						owner.cameraImages(function(src) {
+							if(callback) callback(src);
+						});
+
+						break;
+					case 2:
+						//打开相册 
+						owner.galleryImages(1, function(src) {
+							if(callback) callback(src);
+						});
+						break;
+					default:
+						break;
+				}
+			}, false);
+		}
+	}
+	/*从相册获取图片*/
+	owner.galleryImages = function(num, callback) {
+		// 从相册中选择图片
+		plus.gallery.pick(function(e) {
+			var fileSrc = e.files[0]
+			//				压缩图片
+				owner.zipPic(fileSrc,callback)
+//			if(callback) callback(fileSrc);
+		}, function(e) {
+			plus.nativeUI.closeWaiting();
+			console.log("取消选择图片");
+		}, {
+			filter: "image",
+			multiple: true,
+			maximum: 1,
+			system: false,
+			onmaxed: function() {
+				plus.nativeUI.alert('最多只能选择' + num + '张图片');
+			}
+		});
+	}
+	/*拍照获取图片--  单张*/
+	owner.cameraImages = function(callback) {
+		var mobileCamera = plus.camera.getCamera(1);
+		mobileCamera.captureImage(function(e) {
+			plus.io.resolveLocalFileSystemURL(e, function(entry) {
+				var path = entry.toLocalURL() 			
+//				压缩图片
+				owner.zipPic(path,callback)
+//				if(callback) callback(path);
+			}, function(err) {
+				console.log("读取拍照文件错误");
+			});
+		}, function(e) {
+			console.log( e);
+		}, function() {
+			filename: '_doc/' + new Date().getTime() + '.jpg';
+		});
+	}
+//	压缩图片
+	owner.zipPic= function(src,callback){
+//		console.log('ZIP--'+src)
+		plus.zip.compressImage({
+				src: src,
+				dst: '_doc/zip_' + src.substr(src.lastIndexOf('/') + 1),
+				overwrite: true,
+				quality: 100,
+				format:'jpg'
+			}, function(zip) {
+				plus.nativeUI.closeWaiting();
+				if(callback)(callback(zip.target))
+			}, function() {
+				plus.nativeUI.closeWaiting();
+				mui.toast('压缩失败！');
+			})
+	}
+	
+	/*base64*/
+	owner.getBase64Image = function(img) {
+		var canvas = document.createElement("canvas"); //创建canvas DOM元素，并设置其宽高和图片一样
+		canvas.width = img.width;
+		canvas.height = img.height;
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0, img.width, img.height); //使用画布画图
+		var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase(); //动态截取图片的格式
+		var dataURL = canvas.toDataURL("image/jpg"); //返回的是一串Base64编码的URL并指定格式
+		return dataURL;
+	}
+
+	owner.unpload64 = function(src, size, callback) {
+		size = size || 1
+		plus.nativeUI.showWaiting('图片上传中，请耐心等待');
+		var Info = {
+			image_base64: src,
+			file_name: '.jpg',
+			foldName: plus.storage.getItem('userName'),
+			size: size
+		}
+
+		mui.ajax(owner.baseUrl + '/api/Upload/UploadByBase64', {
+			data: Info,
+			// dataType:'json',//服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			timeout: 10000, //超时时间设置为10秒；
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			success: function(result) {
+
+				if(callback) callback(result.Data);
+
+				mui.later(function() {
+					plus.nativeUI.closeWaiting();
+				}, 1500)
+			},
+			error: function(xhr, type, errorThrown) {
+				plus.nativeUI.closeWaiting();
+				plus.nativeUI.toast('网络不稳定，请重新上传')
+			}
+		})
+	}
+
+}(window.Clip = {}));
---
+++ unpackage/resources/ios.app.mhhCustom/www/js/system.js
@@ -0,0 +1,207 @@
+(function(owner) {　
+
+	owner.baseUrl = "https://mobile.mhh999.com"
+
+	//	owner.pro = document.getElementById('versionTxt')
+	//	owner.mask = document.getElementById('version')
+	//		记录当前的版本信息
+	owner.versionMsg = '';
+	owner.LaunchWebview = null;
+	owner.update = null;
+	//1是不需要更新
+	//2是整包更新
+	//3是差量包升级
+
+	//***************** 系统更新   ***************************
+	//当前版本信息
+	owner.versionInit = function(callback) {
+		owner.LaunchWebview = plus.webview.getLaunchWebview()
+		owner.update = plus.webview.create('./updating.html', 'updating')
+		plus.runtime.getProperty(plus.runtime.appid, function(inf) {
+			owner.wgtVer = inf.version;
+			var checkUrl = owner.baseUrl + "/api/Update/Check";
+			//	    检查版本	    
+			owner.checkUpdate(checkUrl, owner.wgtVer, callback);
+		});
+	}
+
+	// 判断版本信息
+	owner.checkUpdate = function(checkUrl, curversion, callback) {
+		//		plus.nativeUI.showWaiting("检测更新...");
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function() {
+			switch(xhr.readyState) {
+				case 4:
+					plus.nativeUI.closeWaiting();
+					if(xhr.status == 200) {
+						//						console.log(xhr.responseText)
+						owner.versionMsg = JSON.parse(xhr.responseText).Data;
+						if(!JSON.parse(xhr.responseText).Success) {
+							//							没有最新版本
+							if(callback) callback(true)
+							return;
+						} else {
+							var newVer = owner.versionMsg.version;
+							// 有更新包
+							owner.checkSystem(callback)
+						}
+					} else {}
+					break;
+				default:
+					break;
+			}
+		}
+
+		xhr.open('GET', checkUrl + '?version=' + curversion + '&type=' + 1, true);
+		xhr.setRequestHeader("content-type", "application/json");
+
+		xhr.send();
+	}
+
+	//系统检测
+	owner.checkSystem = function(callback) {
+		
+		if(plus.os.name == 'Android') { // Android 用户    
+			　　　　　　
+			if(owner.versionMsg.Android == 1) {
+				if(callback) callback(true) //可以调用自动登陆
+				return false;
+			} else if(owner.versionMsg.Android == 2) {
+				plus.nativeUI.confirm("发现新版本,立即升级", function(e) {
+					if(e.index == 0) {
+						owner.createDownload();
+					} else {
+						if(callback) callback(true)
+					}
+				}, '马嘿嘿提醒你');
+			} else {
+				owner.downWgt(1);
+			}
+		} else {
+			if(owner.versionMsg.iOS == 1) {
+				if(callback) callback(true) //可以调用自动登陆   
+				return false;
+			} else if(owner.versionMsg.iOS == 2) {
+				plus.nativeUI.confirm("发现新版本,立即前往AppStore", function(e) {
+					if(e.index == 0) {
+						plus.runtime.openURL(owner.versionMsg.ipaURL);
+					} else {
+						if(callback) callback(true)
+					}
+				}, '马嘿嘿提醒你');
+			} else {
+				owner.downWgt(2);
+			}
+		}
+	}
+
+	//服务器直接下载安卓整包
+	owner.createDownload = function() {
+		//		alert(owner.versionMsg.apkURL)
+		var dtask = plus.downloader.createDownload(owner.versionMsg.apkURL, {
+			filename: '_doc/download/'
+		}, function(d, status) {
+			// 下载完成
+			if(status == 200) {
+				plus.runtime.install(d.filename, {}, function(widgetInfo) {}, function(DOMException) {});
+			} else {
+				alert("更新失败");
+			}
+		});
+		dtask.start();
+	}
+
+	//	下载差量升级包		
+	owner.downWgt = function(key) {
+		
+		// 下载wgt文件
+		var wgtUrl;
+		switch(key) {
+			case 1:
+				wgtUrl = owner.versionMsg.apkURL;
+				break;
+			case 2:
+				wgtUrl = owner.versionMsg.ipaURL;
+				break;
+			default:
+				break;
+		}
+
+		//		alert(wgtUrl)
+		var tasks = plus.downloader.createDownload(wgtUrl, {
+			filename: "_doc/update/"
+		}, function(d, status) {
+			//			console.log(status)
+			if(status == 200) {
+				//				owner.mask.style.display = "none";
+				//				owner.LaunchWebview.setStyle({
+				//						mask: 'none'
+				//					});
+				owner.update.show()
+			} else {
+				plus.nativeUI.alert("下载升级文件失败！");
+				//				owner.mask.style.display = "none";
+				//				owner.LaunchWebview.setStyle({
+				//						mask: 'none'
+				//					});
+				owner.update.show()
+			}
+		});
+
+		tasks.addEventListener("statechanged", function(download, status) {
+			switch(download.state) {
+				case 2:
+					owner.update.evalJS('text("已连接到服务器")')
+					//					owner.mask.style.display = "block";
+					//					owner.LaunchWebview.setStyle({
+					//						mask: 'rgba(0,0,0,.7)'
+					//					});
+					owner.update.show()
+
+					plus.nativeUI.toast("系统更新中,请稍后...");
+					break;
+				case 3:
+					var percent = download.downloadedSize / download.totalSize * 100;
+					//					owner.pro.innerText = "已下载 " + parseInt(percent) + "%";
+					owner.update.evalJS('text("已下载' + parseInt(percent) + '%")')
+
+					break;
+				case 4:
+					//					owner.pro.innerText = "下载完成";		
+					owner.update.evalJS('text("下载完成")')
+					if(plus.os.name == 'Android') {
+						owner.installWgt(plus.io.convertLocalFileSystemURL(download.filename))
+					} else {
+						//						alert(download.filename)
+						owner.installWgt(download.filename);
+					}
+					// 安装wgt包
+					break;
+			}
+		});
+		tasks.start();
+	}
+
+	//		安装差量升级包
+	owner.installWgt = function(path) {
+		plus.nativeUI.showWaiting("安装升级文件文件...");
+		plus.runtime.install(path, {}, function() {
+			plus.nativeUI.closeWaiting();
+			//				console.log("安装升级文件文件成功！");
+			plus.nativeUI.alert("应用资源更新完成！", function() {
+				plus.runtime.restart();
+			});
+		}, function(e) {
+			plus.nativeUI.closeWaiting();
+			//				console.log("安装wgt文件失败[" + e.code + "]：" + e.message);
+			plus.nativeUI.alert("安装wgt文件失败[" + e.code + "]：" + e.message);
+			//			owner.mask.style.display = "none";
+			//			owner.LaunchWebview.setStyle({
+			//						mask: 'none'
+			//					});
+			owner.update.close()
+		});
+	}
+
+}(window.systemOS = {}));
